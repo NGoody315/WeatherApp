5e50c64631586a3723db833072ee7758adada813
Nikki G <nikki.goodridge@gmail.com>
2021-01-30 18:45:02 -0500

    added humidity, wind speed and other first box content

--- assests/js/script.js
+++ assests/js/script.js
@@ -3,7 +3,12 @@
 var searchBtn = document.getElementById("btn");
 
 //event listeners
-searchBtn.addEventListener("click", getWeather)
+searchBtn.addEventListener("click", getTotalWeather)
+
+function getTotalWeather (){
+    getFiveDay();
+    getWeather();
+}
 
 function getWeather() {
     var searchTerm = document.querySelector("#searchTerm").value;
@@ -17,11 +22,35 @@ function getWeather() {
     .then(function(response){
         console.log(response)
 
-        var responseContainerEl = document.querySelector("#temp-text");
-
+        var responseContainerEl = document.querySelector("#city-name");
+        responseContainerEl.innerHTML = "<h1>" + searchTerm + "</h1>"
+        //add date code here
         document.querySelector('#temp-text').innerHTML = '<h2>' + response.main.temp + ' C </h2>';
-
-
-        //responseContainerEl.appendChild(getTempText);
+        document.querySelector('#humidity').innerHTML = '<h4> Humidity : ' + response.main.humidity + '</h4>'
+        document.querySelector('#wind').innerHTML = '<h4> Wind Speed : ' + response.wind.speed + '</h4>'
+        
+        // return fetch(
+        //     'http://api.openweathermap.org/data/2.5/uvi?lat=' + response.coord.lat + '&lon=' + response.coord.lon + '&appid=c181cbcbd0af0c6eef9a97496c761d1b'
+        // )
+        
+    
     })
 };
+
+function getFiveDay () {
+    var searchTerm = document.querySelector("#searchTerm").value;
+
+    fetch (
+        "http://api.openweathermap.org/data/2.5/forecast?q=" + searchTerm + "&cnt=5&appid=c181cbcbd0af0c6eef9a97496c761d1b"
+        )
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(response){
+            for(var i = 0; i <response.length; i++) {
+                
+            }
+    
+        })
+    }
+    
